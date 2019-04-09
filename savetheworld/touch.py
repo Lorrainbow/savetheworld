52304dbb0b35bd3c8e27bb78d5548db3545008d6
Phil Underwood <beardydoc@gmail.com>
2019-04-09 23:04:57 +0100

    Initial upload

---
+++ savetheworld/touch.py
@@ -0,0 +1,31 @@
+from touchphat import *
+
+_status = [False]*6
+_names = ["BACK","A","B","C","D","ENTER"]
+
+def _pressed(pad):
+    global _status
+    _status[pad] = True
+    
+def _released(pad):
+    global _status
+    _status[pad] = False
+    
+def touched(pad):
+    """Returns True if the pad is touched
+    Pad can be a number (0-5), a name ("BACK","A","B","C","D","ENTER"),
+    or "ANY" (in which case it returns true if any pad is touched)"""
+    if str(pad).upper()=="ANY":
+        return any(_status)
+    try:
+        index = int(pad)
+    except ValueError:
+        try:
+            index = _names.index(str(pad).upper())
+        except ValueError:
+            raise ValueError("{} is not the name of a pad".format(pad))
+            
+for i in range(6):
+    on_touch(i, lambda: _pressed(i))
+    on_release(i, lambda: _released(i))
+        
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SaveTheWorld',
+    version='0.1',
+    packages=['savetheworld',],
+    license='GPL v3',
+    long_description=open('README.md').read(),
+)
