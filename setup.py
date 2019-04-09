b635713a82ba174d0cba812238a47e322650cdfc
Phil Underwood <beardydoc@gmail.com>
2019-04-09 23:17:23 +0100

    Upgrade to setuptools, add dependency...

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='SaveTheWorld',
-    version='0.1',
-    packages=['savetheworld',],
-    license='GPL v3',
-    long_description=open('README.md').read(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="savetheworld",
+    version="0.0.1",
+    author="Phil Underwood",
+    author_email="beardydoc@gmail.com",
+    description="Useful code for the 'Save the World with code' book",
+    long_description=long_description,
+    url="https://github.com/furbrain/savetheworld",
+    packages=setuptools.find_packages(),
+    install_requires=["touchphat"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
