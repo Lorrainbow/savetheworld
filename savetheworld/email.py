bf8faf86d7e50405ae2eb637a7e7941c848d9743
Phil Underwood <beardydoc@gmail.com>
2019-11-10 17:13:13 +0000

    Move sendFile into email.py for simplicity

--- savetheworld/email.py
+++ savetheworld/email.py
@@ -1,5 +1,10 @@
+import os
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 import ssl
+
 
 def send(from_email, to_email, message, password, server="smtp.gmail.com", port=465):
 	"""
@@ -13,3 +18,21 @@ def send(from_email, to_email, message, password, server="smtp.gmail.com", port=
 		connection.login(from_email, password)
 		connection.sendmail(from_email, to_email, message)
 
+def sendFile(from_email, to_email, message, subject, password, attachment, server="smtp.gmail.com", port=465):
+	#setup the email
+	msg = MIMEMultipart()
+	msg['Subject'] = subject
+	msg['From'] = from_email
+	msg['To'] = to_email
+	text = MIMEText(message)	
+	msg.attach(text)
+	
+  #attach an image
+	img_data = open(attachment, 'rb').read()
+	image = MIMEImage(img_data, name=os.path.basename(attachment))
+	msg.attach(image)		
+	
+	#send over ssl
+	with smtplib.SMTP_SSL(server, port, context = ssl.create_default_context()) as connection:
+		connection.login(from_email, password)
+		connection.sendmail(from_email, to_email, msg.as_string())
